8268521768cbfc377c22b3c938f9f134b2ac2d10
Lars Eggert <lars@eggert.org>
2021-03-26 17:54:23 +0200

    Add license

--- ietf-reviewtool.py
+++ ietf-reviewtool.py
@@ -2,6 +2,23 @@
 
 """
 Review tool for IETF documents.
+
+Copyright (C) 2021  Lars Eggert
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+SPDX-License-Identifier: GPL-2.0
 """
 
 import argparse
@@ -612,7 +629,8 @@ def parse_args() -> dict:
         epilog="""In order to operate offline and to speed up operation,
             if you rsync the the various IETF documents onto your local disk,
             set these environment variables to use your local caches as much as
-            possible: IETF_RFCS, IETF_IDS, IETF_CHARTERS""",
+            possible: IETF_CHARTERS, IETF_CONFLICT_REVIEWS, IETF_IDS,
+            IETF_RFCS, IETF_STATUS_CHANGES""",
     )
 
     main_parser.add_argument(
