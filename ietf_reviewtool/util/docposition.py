bfa738e548851fb0faedee9f264a33a3e009bb30
Lars Eggert <lars@eggert.org>
2023-04-13 16:31:04 +0300

    fix: Scan acknowledgements for references (#17)
    Fixes #16

--- ietf_reviewtool/util/docposition.py
+++ ietf_reviewtool/util/docposition.py
@@ -5,7 +5,7 @@ import re
 # pattern matching section headings
 SECTION_PATTERN = re.compile(
     r"""^(?:[\- ]\s)?(Abstract|Status\sof\sThis\sMemo|Copyright\sNotice|
-        Editorial\sNote|Table\sof\sContents|
+        Editorial\sNote|Table\sof\sContents|Acknowledge?ments?|
         (?:(?:Non-)Normative\s|Informative\s)?References?|
         Author(?:'?s?'?)?\sAddress(?:es)?|
         (?:Appendix\s+)?[\dA-Z]+(?:\.\d+)*\.?\s|
